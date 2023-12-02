fbff1ee3956eb60f9d85b46689548de8ddcbd890
Kartik Rajput <kartikvirendrar@Kartiks-MacBook-Air.local>
2023-12-02 18:49:34 +0530

    minor css changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 'use client';
 import themeDefault from "@/themes/theme"
 import { ThemeProvider } from "@emotion/react"
+import "./index.css";
 
 // export const metadata = {
 //   title: 'Anudesh',
@@ -11,7 +12,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <ThemeProvider theme={themeDefault}>{children}</ThemeProvider>
+        <ThemeProvider theme={themeDefault}>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
