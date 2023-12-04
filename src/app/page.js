37ba9b1314431e37fa2d48efe0fa55a32016ab94
Kartik Rajput <kartikvirendrar@Kartiks-MacBook-Air.local>
2023-12-04 11:47:35 +0530

    add datasets page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
     <main>
       <h1>Anudesh</h1>
-      <h3><a href="/organizations">Organization</a></h3>
-      <h3><a href="/login">Login</a></h3>
-      <h3><a href="/invite/500">Sign-Up</a></h3>
-      <h3><a href="/forgot-password">Forgot Password</a></h3>
+      <h3><Link href="/organizations">Organization</Link></h3>
+      <h3><Link href="/login">Login</Link></h3>
+      <h3><Link href="/invite/500">Sign-Up</Link></h3>
+      <h3><Link href="/forgot-password">Forgot Password</Link></h3>
+      <h3><Link href="/dataset">Dataset</Link></h3>
     </main>
   )
 }  
