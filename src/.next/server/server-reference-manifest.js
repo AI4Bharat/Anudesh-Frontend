351757879b876d4ff73dab0be8e4224bc3c5f912
sagarika-padmanaban <sagarikapd@gmail.com>
2023-12-14 16:54:06 +0530

    resolved issues

--- src/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"K+K9Emon3MfaK5vbsxCS3By0/8bpkw2lnHWX358tKVo=\"}"
--- src/out/_next/static/chunks/app/page-f63bed185ea69eae.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{90196:function(n,e,u){Promise.resolve().then(u.t.bind(u,92054,23))}},function(n){n.O(0,[54,115,835,744],function(){return n(n.s=90196)}),_N_E=n.O()}]);
