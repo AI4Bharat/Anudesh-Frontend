9f1de48a661db1fca027b42d7dd5440bbc194fbf
Kartik Virendra Rajput <88619994+kartikvirendrar@users.noreply.github.com>
2023-12-14 10:33:36 +0530

    env file changes

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth,GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MSG_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth=getAuth(app);
+export const googleAuthProvider=new GoogleAuthProvider();
