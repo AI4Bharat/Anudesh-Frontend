d308dc57c4e4ca9f8125ba1982c1f8ac7ca72acb
sagarika-padmanaban <sagarikapd@gmail.com>
2024-01-05 11:32:29 +0530

    added new changes in notify

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@
 import themeDefault from "../themes/theme"
 import { ThemeProvider } from "@emotion/react"
 import {Provider} from 'react-redux';
-import store from "./redux/store/store";
+// import store from "./redux/store/store";
 import "./index.css";
 // export const metadata = {
 //   title: 'Anudesh',
@@ -13,11 +13,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Provider store={store}>
+        {/* <Provider store={store}> */}
         <ThemeProvider theme={themeDefault}>
         {children}
         </ThemeProvider>
-        </Provider>
+        {/* </Provider> */}
       </body>
     </html>
   )
--- src/app/redux/actions/UserManagement/Logout.js
+++
@@ -1,5 +0,0 @@
-export default function Logout() {
-    return {
-        type: "LOGOUT",
-    }
-}
