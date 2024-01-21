55e57f7b5e732fcf2995b4374138300c40366793
kartikvirendrar <kartikrajput357@gmail.com>
2024-01-21 17:37:15 +0530

    fixed login page css

--- src/app/ui/pages/login/login.js
+++ src/app/ui/pages/login/login.js
@@ -258,16 +258,16 @@ export default function Login() {
                 >
                     <AppInfo />
                 </Grid>
-                <Grid item xs={12} sm={9} md={9} lg={9} className="parent">
+                <Grid item xs={12} sm={9} md={9} lg={9} style={{flex:"auto", margin:"auto"}}>
                     <form autoComplete="off">{renderCardContent()}
                     </form>
-                    <div className="w-1/3 flex items-center justify-between my-4">
+                    <div className="w-1/3 flex items-center justify-between my-4 m-auto">
                         <span className="border-b w-1/4"></span>
                         <a href="#" className="text-xs text-center text-gray-500 uppercase">or login with google</a>
                         <span className="border-b w-1/4"></span>
                     </div>
 
-                    <button onClick={() => googleLogin()} className="w-2/5 flex items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg hover:bg-gray-50">
+                    <button onClick={() => googleLogin()} className="w-2/5 flex items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg hover:bg-gray-50 m-auto">
                         <div className="px-4 py-2">
                             <svg className="w-6 h-6" viewBox="0 0 40 40">
                                 <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#FFC107" />
