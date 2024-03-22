c810880d88f373f8e3d9d9c96df919aa7324a05f
sagarika-padmanaban <sagarikapd@gmail.com>
2024-03-22 09:42:10 +0530

    resolved issues

--- src/app/ui/pages/chat/config.js
+++ src/app/ui/pages/chat/config.js
@@ -4,15 +4,15 @@ export const welcomeText = {
 
 export const card1 = {
     heading: 'What is Anudesh?',
-    content: 'Anudesh is an open-source platform dedicated to advancingthe development of state-of-the-art Language Model Modelsfor Indian languages. Our mission is to democratize accessto advanced natural language processing technologies bycreating high-quality conversational data. Create an accountand get started today!'
+    content: 'Anudesh is an open-source platform dedicated to advancingthe development of state-of-the-art Language Model Modelsfor Indian languages. '
 } 
 
 export const card2 = {
     heading: 'How Can You Help?',
-    content: 'You can help us by getting by exploring our projects and contributing to our repositories. We look forward to collaborating with you! You can help us collect data based on instructiional prompts, rate the performance of models, evaluate model responses, do preferential ranking amongst models used, and analyse data using various metrics.'
+    content: 'You can help us collect data based on instructional prompts, rate the performance of models, evaluate model responses, do preferential ranking amongst models used, and analyse data using various metrics.'
 }
 
 export const card3 = {
     heading: 'Why Should You Contribute?',
-    content: 'By contributing to Anudesh, you can play a vital role in enhancing language understanding and generation capabilities in Indian languages. Whether you are a linguist, developer, or language enthusiast, there are many ways to get involved and make an impact. Join us in our journey to empower individuals and communities.'
+    content: 'By contributing to Anudesh, you can play a vital role in enhancing language understanding and generation capabilities in Indian languages.'
 }
