0649ae304a851dce59541558ad68d03d1b5a63ff
kartikvirendrar <kartikrajput357@gmail.com>
2024-04-18 14:18:23 +0530

    fixed partial update qpi call

--- src/app/ui/pages/chat/SuperCheckerPage.jsx
+++ src/app/ui/pages/chat/SuperCheckerPage.jsx
@@ -379,6 +379,7 @@ const SuperCheckerPage = () => {
       ["draft", "skipped", "rejected","labeled"].includes(value) ||
       (["validated", "validated_with_changes"].includes(value) )
     ) {
+      delete PatchAPIdata.auto_save;
       if(value === "rejected") PatchAPIdata["result"] = [];
       const TaskObj = new PatchAnnotationAPI(id, PatchAPIdata);
       const res = await fetch(TaskObj.apiEndPoint(), {
