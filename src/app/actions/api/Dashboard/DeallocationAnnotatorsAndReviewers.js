c220c92c32d5b08f3db1f7594866a4329a6bbca8
sagarika-padmanaban <sagarikapd@gmail.com>
2024-05-28 00:05:18 +0530

    added new changes

--- src/app/actions/api/Dashboard/DeallocationAnnotatorsAndReviewers.js
+++ src/app/actions/api/Dashboard/DeallocationAnnotatorsAndReviewers.js
@@ -8,7 +8,7 @@
 
  export default class DeallocationAnnotatorsAndReviewersAPI extends API {
    constructor(projectId,radiobutton,annotatorsUser,reviewerssUser,annotationStatus,reviewStatus,superCheckUser,SuperCheckStatus,projectObj, timeout = 2000) {
-    super("GET", timeout, false);
+    super("POST", timeout, false);
      this.projectObj = projectObj;
      const queryString = radiobutton === "annotation" ? `unassign_tasks/?annotator_id=${annotatorsUser}&annotation_status=["${annotationStatus}"]` : radiobutton === "review"? `unassign_review_tasks/?reviewer_id=${reviewerssUser}&review_status=["${reviewStatus}"]`:`unassign_supercheck_tasks/?superchecker_id=${superCheckUser}&supercheck_status=["${SuperCheckStatus}"]`;
      this.endpoint = `${super.apiEndPointAuto()}${ENDPOINTS.getProjects}${projectId}/${queryString}`;
