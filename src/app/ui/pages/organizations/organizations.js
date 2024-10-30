6c5ba1c24afaaf10ccd3dc601fa15237c1c6514c
sagarika-padmanaban <sagarikapd@gmail.com>
2024-10-30 18:27:04 +0530

    fixex

--- src/app/ui/pages/organizations/organizations.js
+++ src/app/ui/pages/organizations/organizations.js
@@ -1,24 +1,34 @@
-'use client';
+"use client";
 import DetailsViewPage from "../workspace/DetailsViewPage";
 import componentType from "../../../../config/PageType";
-import Spinner from "@/components/common/Spinner"
+import Spinner from "@/components/common/Spinner";
 import React from "react";
 import { useSelector } from "react-redux";
 
 const MyOrganization = () => {
-    const apiLoading = useSelector((state)=> state.getLoggedInData?.data.organization === undefined)
-    const organizationDetails = useSelector(state=>state.getLoggedInData?.data.organization);
-    return (
-        <React.Fragment>
-        {apiLoading ? <Spinner /> :
+  const apiLoading = useSelector(
+    (state) => state.getLoggedInData?.data.organization === undefined,
+  );
+  const organizationDetails = useSelector(
+    (state) => state.getLoggedInData?.data.organization,
+  );
+  return (
+    <div>
+      {apiLoading ? (
+        <Spinner />
+      ) : (
         <DetailsViewPage
-            title={organizationDetails && organizationDetails.title}
-            createdBy={organizationDetails && organizationDetails.created_by && organizationDetails.created_by.username}
-            pageType={componentType.Type_Organization}
+          title={organizationDetails && organizationDetails.title}
+          createdBy={
+            organizationDetails &&
+            organizationDetails.created_by &&
+            organizationDetails.created_by.username
+          }
+          pageType={componentType.Type_Organization}
         />
-        }
-        </React.Fragment>
-    )
-}
+      )}
+    </div>
+  );
+};
 
 export default MyOrganization;
