b424d6ac036e3d98191218c2c617d9298381a207
Ishvinder Sethi <ishvindersethi22@gmail.com>
2024-12-19 14:24:30 +0530

    Update organizationReports.jsx

--- src/components/Tabs/organizationReports.jsx
+++ src/components/Tabs/organizationReports.jsx
@@ -130,7 +130,7 @@ const OrganizationReports = () => {
   //   if (radiobutton === "ProjectReports") {
   //     setProjectTypes([
   //      "ModelOutputEvaluvation",
-  //      "ModelInteractionEvaluvation",
+  //      "ModelInteractionEvaluation",
   //      "InstructionDrivenChat",
   //     ]);
   //     setSelectedType("InstructionDrivenChat");
@@ -142,7 +142,7 @@ const OrganizationReports = () => {
   //   if (radiobutton === "UsersReports") {
   //     setProjectTypes([
   //      "ModelOutputEvaluvation",
-  //      "ModelInteractionEvaluvation",
+  //      "ModelInteractionEvaluation",
   //      "InstructionDrivenChat",
   //      "MultipleInteractionEvaluation",
   //     ]);
