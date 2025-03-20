a40204d4cac14f51a79288d99bf0c78e9bd99883
dhaval-codes <nimisha25dhaval@gmail.com>
2025-03-20 20:07:59 +0530

    Integrated Frontend Testing and Some other changes as well

--- test/login.spec.ts
+++ test/login.spec.ts
@@ -1,57 +1,49 @@
 import { test, expect } from "@playwright/test";
 
+const TEST_EMAIL = process.env.NEXT_PUBLIC_TEST_EMAIL;
+const TEST_PASSWORD = process.env.NEXT_PUBLIC_TEST_PASSWORD;
+
 test("Login flow", async ({ page }) => {
-  // Navigate to login page
   await page.goto("http://localhost:3000/#/login");
 
-  // Log requests and responses to debug
   page.on("request", (request) => console.log("Request:", request.url()));
   page.on("response", (response) =>
     console.log("Response:", response.url(), response.status()),
   );
 
-  // Wait for the correct login request
   const loginResponse = page.waitForResponse(
     (response) =>
       response.request().method() === "POST" &&
-      response.url().includes("/users/auth/jwt/create") && // Ensure this is correct
+      response.url().includes("/users/auth/jwt/create") &&
       response.status() === 200,
-    { timeout: 60000 }, // Increase timeout
+    { timeout: 60000 },
   );
 
-  // Fill in credentials
-  await page.fill('input[name="email"]', "test_annotator1@anudesh.org");
-  await page.fill('input[name="password"]', "anudesh_admin@123");
+  await page.fill('input[name="email"]', TEST_EMAIL);
+  await page.fill('input[name="password"]', TEST_PASSWORD);
 
-  // Click login button
   await page.click('button[type="submit"]');
 
-  // Wait for login API response
   const response = await loginResponse;
   const responseBody = await response.json();
 
-  // Check if access token is received
   expect(responseBody.access).toBeDefined();
 
-  // Wait for authentication to complete
   await page.waitForLoadState("networkidle");
 
-  // Ensure user is redirected after successful login
   await expect(page).toHaveURL(
-    "http://localhost:3000/?email=test_annotator3%40anudesh.org&password=anudesh_admin%40123#/projects",
+    `http://localhost:3000/?email=${encodeURIComponent(
+      TEST_EMAIL,
+    )}&password=${encodeURIComponent(TEST_PASSWORD)}#/projects`,
   );
 });
 
 test("Forgot Password Flow", async ({ page }) => {
-  // Step 1: Navigate to the Forgot Password page
-  await page.goto("http://localhost:3000/#/forgot-password"); // Replace with your actual URL
+  await page.goto("http://localhost:3000/#/forgot-password");
 
-  // Step 2: Enter a valid email
-  await page.fill("input", "test_annotator3@anudesh.org"); // Adjust the selector if needed
+  await page.fill("input", TEST_EMAIL);
 
-  // Step 3: Click the Reset Password button
-  await page.click("button"); // Adjust the selector if needed
+  await page.click("button");
 
-  // Step 4: Verify redirection to Login Page
-  await expect(page).toHaveURL("http://localhost:3000/#/login"); // Replace with your actual login page URL
+  await expect(page).toHaveURL("http://localhost:3000/#/login");
 });
