b4726e0f825925f7c2823360d9419f5a0dcaa9e0
dhaval-codes <nimisha25dhaval@gmail.com>
2025-03-25 12:44:24 +0530

    added bundle analyzer to test for bundle size

--- next.config.js
+++ next.config.js
@@ -5,11 +5,15 @@
 const CompressionPlugin = require("compression-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
-const nextConfig = ({
+module.exports = withBundleAnalyzer({});
+
+const nextConfig = {
   output: "export",
   images: { unoptimized: true },
-
 
   webpack: (config, { isServer }) => {
     // Enable gzip compression
@@ -20,7 +24,7 @@ const nextConfig = ({
       ...config.optimization,
       minimize: true,
       mergeDuplicateChunks: true,
-      moduleIds: "deterministic", 
+      moduleIds: "deterministic",
       minimizer: [
         new TerserPlugin({
           terserOptions: {
@@ -31,7 +35,7 @@ const nextConfig = ({
               passes: 3, // Applies multiple optimizations
               unsafe: true, // Enables more aggressive optimizations
               hoist_funs: true, // Moves function declarations to the top
-              hoist_vars: true, // Moves variable declarations to the top    
+              hoist_vars: true, // Moves variable declarations to the top
             },
             output: {
               comments: false, // Removes comments in production
@@ -45,16 +49,17 @@ const nextConfig = ({
     };
 
     // Enable better chunking
-    config.optimization.splitChunks = {
+    (config.optimization.splitChunks = {
       chunks: "all",
       minSize: 30 * 1024, // 30 KB minimum chunk size
       maxSize: 250 * 1024, // 250 KB max chunk size
       maxInitialRequests: 10, // Allow more parallel requests
-    },
-{    runtimeChunk: "single", // Extract runtime for better caching
-}
+    }),
+      {
+        runtimeChunk: "single", // Extract runtime for better caching
+      };
     return config;
   },
-});
+};
 
 module.exports = nextConfig;
