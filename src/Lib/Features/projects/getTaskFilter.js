41b30d321975c691968ebe58a3ab3f69a45ef78c
tiasaxena <tia.saxena041202@gmail.com>
2025-05-06 13:19:10 +0530

    removed permanent storage for filters per project

--- src/Lib/Features/projects/getTaskFilter.js
+++ src/Lib/Features/projects/getTaskFilter.js
@@ -1,25 +1,39 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { accordionActionsClasses } from "@mui/material";
+import { createSlice } from "@reduxjs/toolkit";
 
 // Define the initial state
 const initialState = {
   data: [],
-  status: 'idle',
+  status: "idle",
   error: null,
 };
 
 const getTaskFilter = createSlice({
-  name: 'getTaskFilter',
+  name: "getTaskFilter",
   initialState,
   reducers: {
-    setTaskFilter : (state, action) => {
-      state.status = 'succeeded';
-      // state.data = action.payload;
-      state.data = state.data.filter(task => task.id !== action.payload.id);
-      state.data.push(action.payload);
+    setTaskFilter: (state, action) => {
+      state.status = "succeeded";
+      const newItem = action.payload;
+
+      const idMatches = state.data.filter((item) => item.id === newItem.id);
+
+      if (idMatches.length === 0) {
+        state.data = [newItem];
+      } else {
+        const index = state.data.findIndex(
+          (item) => item.id === newItem.id && item.type === newItem.type,
+        );
+
+        if (index !== -1) {
+          state.data[index] = newItem;
+        } else {
+          state.data.push(newItem);
+        }
+      }
     },
   },
 });
 
 export const { setTaskFilter } = getTaskFilter.actions;
 export default getTaskFilter.reducer;
-
