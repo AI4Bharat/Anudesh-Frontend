2bf00e122a8e61d2748a693b03d3391e2c131c36
kartikvirendrar <kartikrajput357@gmail.com>
2025-06-12 16:39:24 +0530

    minor fix for lang change

--- src/components/Chat/TextArea.jsx
+++ src/components/Chat/TextArea.jsx
@@ -217,6 +217,7 @@ export default function Textarea({
       }
       {(globalTransliteration || defLang!==null)? (
         <IndicTransliterate
+          key={`indic-${defLang || 'default'}`}
           customApiURL={`${configs.BASE_URL_AUTO}/tasks/xlit-api/generic/transliteration/`}
           enableASR={true}
           asrApiUrl={`${configs.BASE_URL_AUTO}/tasks/asr-api/generic/transcribe`}
