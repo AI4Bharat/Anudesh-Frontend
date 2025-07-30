9f86aff68542f86554216fb42e61a4be5ed97683
munishmangla98 <munishmangla98@gmail.com>
2025-07-30 16:24:58 +0530

    done

--- src/app/ui/pages/workspace/bulkassignmembers.jsx
+++ src/app/ui/pages/workspace/bulkassignmembers.jsx
@@ -180,7 +180,7 @@ const AssignMembersDialog = () => {
         autoHideDuration={6000}
         onClose={() => setResponseMessage('')}
         anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-        sx={{top: '50% !important',left: '50% !important',transform: 'translate(-50%, -50%)',}}
+        sx={{top: '15% !important',left: '50% !important',transform: 'translate(-50%, -50%)',}}
       >
         <Alert severity={responseType} onClose={() => setResponseMessage('')} sx={{
           width: '100%',
