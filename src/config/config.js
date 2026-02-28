9b296d0877106320cad491f478c3f2f3ee435bac
munishmangla98 <munishmangla98@gmail.com>
2026-02-28 22:56:07 +0530

    check

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ const configs = {
   BASE_URL: "https://backend.anudesh.ai4bharat.org",
   BASE_URL_AUTO: process.env.NEXT_PUBLIC_BACKEND_URL
     ? process.env.NEXT_PUBLIC_BACKEND_URL
-       :"https://backend.dev.anudesh.ai4bharat.org",
+       :"http://127.0.0.1:8000",
 };
 
 export default configs;
