58b6172452f8d3d46bc8dd0cb3a15ada2a0f4d7b
Dhara Gorasiya <dharagorasiya@gmail.com>
2026-04-29 09:39:11 +0530

    fix: remove invalid string onLoad handler in React JSX

--- src/app/layout.js
+++ src/app/layout.js
@@ -36,8 +36,6 @@ export default function RootLayout({ children }) {
         <link
           href="https://fonts.googleapis.com/css2?family=Rowdies:wght@400;700&family=Roboto:wght@400;700&display=swap"
           rel="stylesheet"
-          media="print"
-          onLoad="this.onload=null;this.removeAttribute('media');"
         />
       </head>
 
