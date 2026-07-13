4ba3efea8e144bc6d7fafd03fe1e17ab58fddbc4
Rahul Kumar <142306874+Rahul-538@users.noreply.github.com>
2026-07-13 16:10:45 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ const configs = {
   BASE_URL: "https://backend.anudesh.ai4bharat.org",
   BASE_URL_AUTO: process.env.NEXT_PUBLIC_BACKEND_URL
     ? process.env.NEXT_PUBLIC_BACKEND_URL
-       :"https://backend.anudesh.ai4bharat.org",
+       :"https://backend.dev.anudesh.ai4bharat.org",
 };
 
 export default configs;
