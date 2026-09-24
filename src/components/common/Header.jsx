cb68a960e7b5f3e8e55f420626a918d65fbf647d
RenukaPatil07 <renukapatil2607@gmail.com>
2026-09-24 11:16:48 +0530

    Fix notification tab (#745)

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -135,10 +135,10 @@ const translationServices = {
         console.error("Error fetching notifications:", error);
       });
   };
-  const fetchUnreadCount = async () => {
+    const fetchUnreadCount = async () => {
     try {
       let apiObj = new NotificationAPI();
-      const endpoint = `${apiObj.apiEndPoint()}unread`;
+      const endpoint = `${apiObj.apiEndPoint()}/unread`;
       const response = await fetch(endpoint, {
         method: "GET",
         headers: apiObj.getHeaders().headers,
@@ -160,19 +160,21 @@ const translationServices = {
   }, []);
 
 
-  const markAsRead =  (notificationId) => {
-    const task = new NotificationPatchAPI(notificationId);
-    setSelectedNotificationId(notificationId);
-    dispatch(APITransport(task));
-    fetchNotifications();
-  };
+    const markAsRead = async (notificationId) => {
+  const task = new NotificationPatchAPI(notificationId);
+  setSelectedNotificationId(notificationId);
+  await dispatch(APITransport(task));
+  fetchNotifications();
+  fetchUnreadCount();
+};
 
-  const markAllAsRead = () => {
+      const markAllAsRead = async () => {
     const notificationIds = Notification.map((notification) => notification.id);
     const tasks = new NotificationPatchAPI(notificationIds);
     setSelectedNotificationId(notificationIds);
-    dispatch(APITransport(tasks));
+    await dispatch(APITransport(tasks));
     fetchNotifications();
+    fetchUnreadCount();
   };
 
   const handleMarkAllAsReadClick = () => {
@@ -183,9 +185,9 @@ const translationServices = {
     markAsRead(notificationId);
   };
 
-  // useEffect(() => {
-  //   fetchNotifications();
-  // }, [unread,selectedNotificationId]);
+  useEffect(() => {
+  fetchNotifications();
+   }, [unread,selectedNotificationId]);
   
   useEffect(() => {
     getLoggedInUserData();
@@ -267,6 +269,7 @@ const translationServices = {
 
   const handleOpenNotification = (event) => {
     setAnchorElNotification(event.currentTarget);
+    fetchNotifications();
   };
 
   const handleCloseNotification = () => {
@@ -1213,27 +1216,38 @@ useEffect(() => {
                   ))}
                 </Menu>
                 <Menu
-                  sx={{ mt: "45px", display: "flex", flexDirection: "row" }}
-                  id="menu-appbar"
-                  anchorEl={anchorElNotification}
-                  anchorOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: "top",
-                    horizontal: "center",
-                  }}
-                  style={{ overflow: "scroll" }}
-                  open={Boolean(anchorElNotification)}
-                  onClose={handleCloseNotification}
-                >
+                    id="menu-appbar"
+                    anchorEl={anchorElNotification}
+                    anchorOrigin={{
+                      vertical: "top",
+                      horizontal: "right",
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "center",
+                    }}
+                    open={Boolean(anchorElNotification)}
+                    onClose={handleCloseNotification}
+                    PaperProps={{
+                      sx: {
+                        mt: "45px",
+                        width: 500,
+                        maxHeight: 500,
+                        overflowY: "auto",
+                        bgcolor: "background.paper",
+                      },
+                    }}
+                  >
                   <Stack
                     direction="row"
-                    style={{
+                    sx={{
                       justifyContent: "space-between",
                       padding: "0 10px 0 10px",
+                      position: "sticky",
+                      top: 0,
+                      bgcolor: "background.paper",
+                      zIndex: 1,
                     }}
                   >
                     <Typography variant="h4">Notifications</Typography>
